3703ea11c96d9b62df497a9a4d70967b1a53e10e
ahmedabdellatif <ahmedabdellatifhosny@gmaol.com>
2024-12-28 21:48:31 +0200

    Add Vercel Analytics

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,24 +1,24 @@
-
 import Header from "../components/layout/Header";
 import Footer from "../components/layout/Footer";
 
-
-import 'bootstrap/dist/css/bootstrap.min.css';
+import "bootstrap/dist/css/bootstrap.min.css";
 import "./style/globals.css";
+
+import { Analytics } from "@vercel/analytics/react";
 
 export const metadata = {
   title: "Dev Ahmed - Portfolio",
   description: "Portfolio of Dev Ahmed - frontend developer ",
-  icons :{
-    icon : "/images/header/hero-avatar.jpg"
-  }
+  icons: {
+    icon: "/images/header/hero-avatar.jpg",
+  },
 };
-
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en"> 
+    <html lang="en">
       <body>
+        <Analytics />
         <Header />
         {children}
         <Footer />
